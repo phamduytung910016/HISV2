2f6dc51ed7479ba390b84f4035a8d76c902c6b42
Pham Tùng <phamduytung910016@gmail.com>
2022-05-23 14:48:59 +0700

    add sout

--- src/main/java/com/ibme/pacs/TestGit.java
+++ src/main/java/com/ibme/pacs/TestGit.java
@@ -3,5 +3,6 @@ package com.ibme.pacs;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Test git");
+        System.out.println("Test git 2");
     }
 }
